35bae0bf06bae84a7b4ee141d0a196fd477dc9d4
Deen Dela Cruz <120246740+DeenDelaCruz@users.noreply.github.com>
2025-03-03 08:58:12 +0800

    Update UnliCallTextPackage.java

--- UnliCallTextPackage.java
+++ UnliCallTextPackage.java
@@ -11,11 +11,11 @@ public class UnliCallTextPackage implements UnliCallsTextOffer{
     private String getUnliCallTextOffer(String telcoName) {
         switch (telcoName) {
             case "Smart":
-                return "And they do not offer any free calls or text.";
+                return "However, they do not offer any free calls or texts, and you will be charged per use.";
             case "Globe":
-                return "This plan comes with unlimited calls and texts to subscribers within their network.";
+                return "This plan comes with unlimited calls and texts to subscribers within their network. Calls and texts to other networks are charged extra.";
             case "Ditto":
-                return "This plan has unlimited calls and texts to all networks.";
+                return "This plan includes unlimited calls and texts to all networks within the country.";
             default:
                 return "Unknown";
         }
